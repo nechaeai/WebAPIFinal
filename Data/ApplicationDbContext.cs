363da9288cf1e424b793d92a841a9430e064d343
Nhi Nguyen <nhing17899@gmail.com>
2023-07-30 15:33:56 -0400

    Fix: Remove unneccessary files + update gitignore

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,8 +12,10 @@ namespace WebAPIFinal.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<FavoritePet>().HasData(
-                new FavoritePet { Id = 1, Name = "Meow", Owner = "Nhi", Breed = "British Long Hair", DOB = new DateTime(2022, 05, 07), FavoriteFood = "Chicken Breast"},
-                new FavoritePet { Id = 2, Name = "Zeus", Owner = "Samir", Breed = "Australian Shepherd", DOB = new DateTime(2010, 12, 18), FavoriteFood = "Peanut butter"}
+                new FavoritePet { Id = 1, Name = "Meow", Owner = "Nhi", Breed = "British Long Hair", DOB = new DateTime(2022, 5, 7), FavoriteFood = "Chicken Breast"},
+                new FavoritePet { Id = 2, Name = "Zeus", Owner = "Samir", Breed = "Australian Shepherd", DOB = new DateTime(2010, 12, 18), FavoriteFood = "Peanut butter"},
+                new FavoritePet { Id = 3, Name = "Moss", Owner = "Shane", Breed = "Yorki", DOB = new DateTime(2004, 5, 26), FavoriteFood = "Biscuits"},
+                new FavoritePet { Id = 4, Name = "Moss", Owner = "Shane", Breed = "Yorki", DOB = new DateTime(2004, 5, 26), FavoriteFood = "Biscuits"}
             );
         }
         public DbSet<TeamMember> TeamMembers { get; set; }
