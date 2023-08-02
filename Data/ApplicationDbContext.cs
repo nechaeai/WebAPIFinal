47a5c3a1f0efc50b9fffdc8a8c7bb0c6993bbaf3
thund <thund@MSI>
2023-08-02 18:40:08 -0400

    controller added and DBContext updated

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,11 +12,11 @@ namespace WebAPIFinal.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<FavoritePet>().HasData(
-                new FavoritePet { Id = 1, Name = "Meow", Owner = "Nhi", Breed = "British Long Hair", DOB = new DateTime(2022, 5, 7), FavoriteFood = "Chicken Breast"},
-                new FavoritePet { Id = 2, Name = "Zeus", Owner = "Samir", Breed = "Australian Shepherd", DOB = new DateTime(2010, 12, 18), FavoriteFood = "Peanut butter"},
-                new FavoritePet { Id = 3, Name = "Moss", Owner = "Shane", Breed = "Yorki", DOB = new DateTime(2004, 5, 26), FavoriteFood = "Biscuits"},
-                new FavoritePet { Id = 4, Name = "Moss", Owner = "Shane", Breed = "Yorki", DOB = new DateTime(2004, 5, 26), FavoriteFood = "Biscuits"}
+            modelBuilder.Entity<FavoriteRestaurants>().HasData(
+                new FavoriteRestaurants { Id = 1, Name = "Fuji Steak House", Favorite Food = "volcano roll", location = "4389 Eastgate Square Dr Cincinnati OH", Last Visited = new DateTime(2023,07,01)},
+                new FavoriteRestaurants { Id = 2, Name = "Cava", Favorite Food = "Grain bowl with grilled chicken", location = "13920 Old Saint Augustine Rd FL", Last Visited = new DateTime(2023, 07, 13) },
+                new FavoriteRestaurants { Id = 3, Name = "Bibibop", Favorite Food = "Glass noodle bowl with BBQ beef", location = "228 Calhoun st Cincinnati OH", Last Visited = new DateTime(2023, 07, 27) },
+                new FavoriteRestaurants { Id = 4, Name = "El Nopal", Favorite Food = "Quesadilla", location = "3218 Dixie Hwy Erlanger KY", Last Visited = new DateTime(2023, 07, 27)}
             );
         }
         public DbSet<TeamMember> TeamMembers { get; set; }
